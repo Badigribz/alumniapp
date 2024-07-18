f8d85e8f057bd39975a10a0142f6ea727dcf667d
Gribz <gribzbadi@gmail.com>
2024-07-18 12:06:44 +0300

    rm spatie

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 //put this route and imported it above in an attempt to change the beginning page to login directly 
 Route::get('/', [AuthenticatedSessionController::class, 'create'])->name('login');
+//
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -21,22 +22,22 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::group(['middleware' => ['role:super-admin|admin']], function() {
+// Route::group(['middleware' => ['role:super-admin|admin']], function() {
 
-    Route::resource('permissions', App\Http\Controllers\PermissionController::class);
+//     Route::resource('permissions', App\Http\Controllers\PermissionController::class);
 
-    Route::get('permissions/{permissionId}/delete', [App\Http\Controllers\PermissionController::class, 'destroy']);
+//     Route::get('permissions/{permissionId}/delete', [App\Http\Controllers\PermissionController::class, 'destroy']);
 
 
-    Route::resource('roles', App\Http\Controllers\RoleController::class);
-    Route::get('roles/{roleId}/delete', [App\Http\Controllers\RoleController::class, 'destroy']);
-    Route::get('roles/{roleId}/give-permissions', [App\Http\Controllers\RoleController::class, 'addPermissionToRole']);
-    Route::put('roles/{roleId}/give-permissions', [App\Http\Controllers\RoleController::class, 'givePermissionToRole']);
+//     Route::resource('roles', App\Http\Controllers\RoleController::class);
+//     Route::get('roles/{roleId}/delete', [App\Http\Controllers\RoleController::class, 'destroy']);
+//     Route::get('roles/{roleId}/give-permissions', [App\Http\Controllers\RoleController::class, 'addPermissionToRole']);
+//     Route::put('roles/{roleId}/give-permissions', [App\Http\Controllers\RoleController::class, 'givePermissionToRole']);
 
-    Route::resource('users', App\Http\Controllers\UserController::class);
-    Route::get('users/{userId}/delete', [App\Http\Controllers\UserController::class, 'destroy']);
+//     Route::resource('users', App\Http\Controllers\UserController::class);
+//     Route::get('users/{userId}/delete', [App\Http\Controllers\UserController::class, 'destroy']);
 
-});
+// });
 
 
 require __DIR__.'/auth.php';
