14060522cbabddc5fe7e78ca8dfb8fde71ddf54b
Gribz <gribzbadi@gmail.com>
2024-07-19 19:52:39 +0300

    trying to colour and size nav colour

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-<nav x-data="{ open: false }" class="bg-red-500 border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-red-500 border-b border-gray-100"style="height:11.8vh">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
